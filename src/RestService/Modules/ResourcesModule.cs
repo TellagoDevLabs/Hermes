1bb1a6b43f6baefcb395e9548cbb27769c1a79af
jfromaniello <devnull@localhost>
2011-05-31 17:05:52 -0300

    extracted registration to modules

---
+++ src/RestService/Modules/ResourcesModule.cs
@@ -0,0 +1,17 @@
+using Autofac;
+
+namespace TellagoStudios.Hermes.RestService.Modules
+{
+    public class ResourcesModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+            builder.RegisterType<GroupsResource>().AsSelf().AsImplementedInterfaces();
+            builder.RegisterType<LogResource>().AsSelf().AsImplementedInterfaces();
+            builder.RegisterType<MessageResource>().AsSelf().AsImplementedInterfaces();
+            builder.RegisterType<SubscriptionResource>().AsSelf().AsImplementedInterfaces();
+            builder.RegisterType<TopicsResource>().AsSelf().AsImplementedInterfaces();
+        }
+    }
+}
---
+++ src/RestService/Modules/ServicesModule.cs
@@ -0,0 +1,76 @@
+using Autofac;
+using TellagoStudios.Hermes.Business.Repository;
+using TellagoStudios.Hermes.Business.Service;
+using TellagoStudios.Hermes.Business.Validator;
+
+namespace TellagoStudios.Hermes.RestService.Modules
+{
+    public class ServicesModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+            
+            builder.RegisterType<GroupService>()
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope()
+                .OnActivated(c =>
+                {
+                    c.Instance.Repository = c.Context.Resolve<IGroupRepository>();
+                    c.Instance.Validator = c.Context.Resolve<GroupValidator>();
+                });
+
+            builder.RegisterType<LogService>()
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope()
+                .OnActivated(c =>
+                {
+                    c.Instance.Repository = c.Context.Resolve<ILogRepository>();
+                });
+
+            builder.RegisterType<MessageService>()
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope()
+                .OnActivated(c =>
+                {
+                    c.Instance.Repository = c.Context.Resolve<IMessageRepository>();
+                    c.Instance.Validator = c.Context.Resolve<MessageValidator>();
+                    c.Instance.GroupService = c.Context.Resolve<IGroupService>();
+                    c.Instance.LogService = c.Context.Resolve<ILogService>();
+                    c.Instance.RetryService = c.Context.Resolve<IRetryService>();
+                    c.Instance.TopicService = c.Context.Resolve<ITopicService>();
+                    c.Instance.SubscriptionService = c.Context.Resolve<ISubscriptionService>();
+
+                });
+
+            builder.RegisterType<RetryService>()
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope()
+                .OnActivated(c =>
+                {
+                    c.Instance.Repository = c.Context.Resolve<IRetryRepository>();
+                    c.Instance.LogService = c.Context.Resolve<ILogService>();
+                });
+
+            builder.RegisterType<SubscriptionService>()
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope()
+                .OnActivated(c =>
+                {
+                    c.Instance.Repository = c.Context.Resolve<ISubscriptionRepository>();
+                    c.Instance.Validator = c.Context.Resolve<SubscriptionValidator>();
+                    c.Instance.GroupService = c.Context.Resolve<IGroupService>();
+                    c.Instance.TopicService = c.Context.Resolve<ITopicService>();
+                });
+
+            builder.RegisterType<TopicService>()
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope()
+                .OnActivated(c =>
+                {
+                    c.Instance.Repository = c.Context.Resolve<ITopicRepository>();
+                    c.Instance.Validator = c.Context.Resolve<TopicValidator>();
+                });
+        }
+    }
+}
