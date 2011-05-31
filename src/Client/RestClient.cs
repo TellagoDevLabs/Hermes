5db430b15c02fd6672a979c67deb4be0068cb582
Silvio Massari <silvio.massari@gmail.com>
2011-05-31 12:14:05 -0300

    WebExceptionHandler was added to RestClient class

--- src/Client/RestClient.cs
+++ src/Client/RestClient.cs
@@ -12,60 +12,61 @@ namespace TellagoStudios.Hermes.Client
     public abstract class RestClient
     {
         protected static Uri BaseAddress { get; set; }
+        public static Action<WebException> WebExceptionHandler { get; set; }
 
-        protected HttpWebResponse GetResponse(string operation, IEnumerable<Header> headers = null)
+        protected HttpWebResponse GetResponse(string operation, IEnumerable<Header> headers = null, Action<WebException> webExceptionHandler = null)
         {
             return Client(operation, "GET", headers)
-                .Send();
+                .Send(webExceptionHandler);
         }
 
-        protected void Get(string operation, IEnumerable<Header> headers = null)
+        protected void Get(string operation, IEnumerable<Header> headers = null, Action<WebException> webExceptionHandler = null)
         {
             Client(operation, "GET", headers)
-                .Send();
+                .Send(webExceptionHandler);
         }
 
-        protected T Get<T>(string operation, IEnumerable<Header> headers = null)
+        protected T Get<T>(string operation, IEnumerable<Header> headers = null, Action<WebException> webExceptionHandler = null)
         {
             return Client(operation, "GET", headers)
-                .Send()
+                .Send(webExceptionHandler)
                 .Deserialize<T>();
         }
-        
-        protected void Put<T>(string operation, T data, IEnumerable<Header> headers = null) 
+
+        protected void Put<T>(string operation, T data, IEnumerable<Header> headers = null, Action<WebException> webExceptionHandler = null) 
         {
             Client(operation, "PUT", headers)
                 .Serialize(data)
-                .Send();
+                .Send(webExceptionHandler);
         }
 
-        protected U Put<T, U>(string operation, T data, IEnumerable<Header> headers = null)
+        protected U Put<T, U>(string operation, T data, IEnumerable<Header> headers = null, Action<WebException> webExceptionHandler = null)
         {
             return Client(operation, "PUT", headers)
                 .Serialize(data)
-                .Send()
+                .Send(webExceptionHandler)
                 .Deserialize<U>();
         }
 
-        protected void Post<T>(string operation, T data, IEnumerable<Header> headers = null) 
+        protected void Post<T>(string operation, T data, IEnumerable<Header> headers = null, Action<WebException> webExceptionHandler = null) 
         {
             Client(operation, "POST", headers)
                 .Serialize(data)
-                .Send();
+                .Send(webExceptionHandler);
         }
 
-        protected U Post<T, U>(string operation, T data, IEnumerable<Header> headers = null)
+        protected U Post<T, U>(string operation, T data, IEnumerable<Header> headers = null, Action<WebException> webExceptionHandler = null)
         {
             return Client(operation, "POST", headers)
                 .Serialize(data)
-                .Send()
+                .Send(webExceptionHandler)
                 .Deserialize<U>();
         }
 
-        protected void Delete(string operation, IEnumerable<Header> headers = null)
+        protected void Delete(string operation, IEnumerable<Header> headers = null, Action<WebException> webExceptionHandler = null)
         {
             Client(operation, "DELETE", headers)
-                .Send();
+                .Send(webExceptionHandler);
         }
 
         private static HttpWebRequest Client(string operation, string method, IEnumerable<Header> headers)
@@ -117,10 +118,27 @@ namespace TellagoStudios.Hermes.Client
 
     static class RestClientExtensions
     {
-        public static HttpWebResponse Send(this HttpWebRequest request)
+        public static HttpWebResponse Send(this HttpWebRequest request, Action<WebException> webExceptionHandler)
         {
-            var response = (HttpWebResponse)request.GetResponse();
-            return response;
+            try
+            {
+                var response = (HttpWebResponse)request.GetResponse();
+                return response;
+            }
+            catch (WebException e)
+            {
+                if (webExceptionHandler != null)
+                {
+                    webExceptionHandler(e);
+                    return null;
+                }
+                if (RestClient.WebExceptionHandler != null)
+                {
+                    RestClient.WebExceptionHandler(e);
+                    return null;
+                }
+                throw;
+            }
         }
 
         static public HttpWebRequest Serialize<T>(this HttpWebRequest request, T data)
@@ -135,13 +153,13 @@ namespace TellagoStudios.Hermes.Client
             }
             else
             {
-                var settings = new XmlWriterSettings {OmitXmlDeclaration = true};
+                var settings = new XmlWriterSettings { OmitXmlDeclaration = true };
                 var writer = XmlWriter.Create(stream, settings);
 
                 var ns = new XmlSerializerNamespaces();
                 ns.Add("", "http://schemas.datacontract.org/2004/07/TellagoStudios.Hermes.RestService.Facade");
 
-                var serializer = new XmlSerializer(typeof (T));
+                var serializer = new XmlSerializer(typeof(T));
                 serializer.Serialize(writer, data, ns);
             }
             return request;
