db3979afc88163f9a09bccef3d2a1cd0172fb7a2
Jose F. Romaniello <devnull@localhost>
2011-06-03 10:45:56 -0300

    implemented topics by group query

---
+++ src/DataAccess/Queries/TopicsByGroup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using TellagoStudios.Hermes.Business.Model;
+using TellagoStudios.Hermes.Business.Queries;
+using TellagoStudios.Hermes.DataAccess.MongoDB;
+
+namespace TellagoStudios.Hermes.DataAccess.Queries
+{
+    public class TopicsByGroup : MongoDbRepository, ITopicsByGroup
+    {
+        private readonly MongoCollection<Topic> topicsCollection;
+
+        public TopicsByGroup(string connectionString) : base(connectionString)
+        {
+            topicsCollection = DB.GetCollection<Topic>(MongoDbConstants.Collections.Topics);
+        }
+
+        public bool HasTopics(Group group)
+        {
+            if(!group.Id.HasValue ) throw new InvalidOperationException("Group without id.");
+            return topicsCollection.Exists(QueryGetByGroup(group.Id.Value));
+        }
+
+        public IEnumerable<Topic> GetTopics(Group group)
+        {
+            if (!group.Id.HasValue) throw new InvalidOperationException("Group without id.");
+            return topicsCollection.Find(QueryGetByGroup(group.Id.Value));
+        }
+
+        private static QueryDocument QueryGetByGroup(Identity groupId)
+        {
+            var query = "{\"GroupId\" : " + groupId.ToBsonString() + "}";
+            return query.ToQueryDocument();
+
+        }
+    }
+}
