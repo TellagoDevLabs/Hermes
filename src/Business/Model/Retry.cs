bd6b0bb6bfb5e07afe6e267863b19b43f5e7b753
Silvio Massari <silvio.massari@gmail.com>
2011-06-09 13:50:04 -0300

    Retry class was updated. It's inherits from EnittyBase

---
+++ src/Business/Model/Retry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TellagoStudios.Hermes.Business.Model
+{
+    public class Retry : EntityBase
+    {
+
+        public Retry()
+        {
+        }
+
+        public Retry(Message message, Subscription subscription)
+        {
+            Message = message;
+            Subscription = subscription;
+        }
+
+        public int Count { get; set; }        
+        // UTC date time when the message was tried to send last time
+        public DateTime UtcLastTry { get; set; }
+
+        public Message Message { get; set; }
+        public Subscription Subscription { get; set; }               
+    }
+}
