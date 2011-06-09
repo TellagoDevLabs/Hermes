1f210044af5a7130b18616dcbbd22a96d4497134
Jose F. Romaniello <devnull@localhost>
2011-06-09 09:08:20 -0300

    added queryvalidator

---
+++ src/DataAccess/Queries/QueryValidator.cs
@@ -0,0 +1,21 @@
+using MongoDB.Driver;
+using TellagoStudios.Hermes.Business.Data.Queries;
+
+namespace TellagoStudios.Hermes.DataAccess.Queries
+{
+    public class QueryValidator : IQueryValidator
+    {
+        public bool IsValid(string query)
+        {
+            try
+            {
+                query.ToQueryDocument();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
