9f4969ed790d07ecdab941edd0addb54c14f7c40
Jose F. Romaniello <devnull@localhost>
2011-06-24 17:00:18 -0300

    added topic statistics to home

--- src/RestService/Controllers/HomeController.cs
+++ src/RestService/Controllers/HomeController.cs
@@ -1,15 +1,21 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using TellagoStudios.Hermes.Business.Data.Queries;
 
 namespace TellagoStudios.Hermes.RestService.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ITopicsStatistics topicsStatistics;
+
+        public HomeController(ITopicsStatistics topicsStatistics)
+        {
+            this.topicsStatistics = topicsStatistics;
+        }
+
         public ActionResult Index()
         {
+            ViewBag.TopicsStatistics = topicsStatistics.Execute();
             return View();
         }
 
