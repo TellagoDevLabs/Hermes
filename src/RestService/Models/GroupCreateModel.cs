3b2fadf41c24c56474fd8a2da4b18f8bc729724c
Silvio Massari <silvio.massari@gmail.com>
2011-06-27 18:19:39 -0300

    CRUD for Group was added to the portal

---
+++ src/RestService/Models/GroupCreateModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TellagoStudios.Hermes.RestService.Models
+{
+    public class GroupCreateModel
+    {
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string ParentId { get; set; }
+
+        public IEnumerable<GroupModel> GroupList { get; set; }
+    }
+}
