072ccf95e67967aa58a9c12585f4181cf9a4c486
Jason Dentler <jasondentler@gmail.com>
2011-07-12 16:39:00 -0500

    Hermes js client

---
+++ src/ExampleWebSubscriber/Scripts/Hermes.js
@@ -0,0 +1,117 @@
+﻿
+function HermesClient(serviceUrl) {
+    if (!(this instanceof HermesClient))
+        return new HermesClient(serviceUrl);
+    if (serviceUrl == null || serviceUrl == '')
+        throw "serviceUrl should not be null or empty";
+
+    this.CreateGroup = function (name, description) {
+        if (name == null || name == '')
+            throw 'Group name is null or empty';
+
+        var data = {
+            name: name,
+            description: description
+        };
+
+        var action = restClient.Post(restClient.Operations.Groups, null, data);
+        var result = $.Deferred();
+
+        action
+            .fail(function(msg) { result.reject(msg); })
+            .done(function(data) {
+                console.log(data);
+                throw "Not implemented: deserializing data in to a group object.";
+            });
+
+        return result.promise();
+    };
+    
+    var restClient = new RestClient();
+    function RestClient() {
+
+        var makeAbsolute = function (base, relativeUrl) {
+            // far from perfect...
+            return base + relativeUrl;
+        };
+
+        var client = function(operation, method, headers) {
+            var url;
+            if (operation == null || operation == '') {
+                url = serviceUrl;
+            } else {
+                url = makeAbsolute(serviceUrl, operation);
+            }
+            return buildRequest(url, method, headers);
+        };
+
+        var buildRequest = function(url, method, headers) {
+            return function(data) {
+                return $.ajax(url, {
+                    type: method,
+                    data: data,
+                    headers: headers
+                });
+            };
+        };
+        
+        this.Operations = {
+            Groups: "groups",
+            Messages: "messages",
+            Subscriptions: "subscriptions",
+            GetGroup: function(id) {
+                return Groups + "/" + id;
+            }
+        };
+        
+        this.Get = function(operation, headers) {
+            return client(operation, "GET", headers)(null);
+        };
+
+        this.Put = function(operation, headers, data) {
+            return client(operation, "PUT", headers)(data);
+        };
+
+        this.Post = function (operation, headers, data) {
+            var createGroupRequest = client(operation, "POST", headers)(data);
+            var promise = $.Deferred();
+
+            createGroupRequest.error(function(msg) {
+                promise.reject(msg);
+            }).success(function(data, status, xhr) {
+                var location = xhr.getResponseHeader('Location');
+                $.get(location).error(function(msg) {
+                    promise.reject(msg);
+                }).success(function(data) {
+                    promise.resolve(data);
+                });
+            });
+
+            return promise.promise();
+        };
+
+        this.Delete = function(operation, headers, data) {
+            return client(operation, "DELETE", headers)(data);
+        };
+    };
+    
+    
+    
+
+
+}
+
+function Group(groupName, groupDescription) {
+    if (!(this instanceof Group))
+        return new Group(groupName, groupDescription);
+    if (groupName == null || groupName == '')
+        throw new "name should not be null or empty";
+
+    this.TryCreateTopic = function (name, description) {
+    };
+
+}
+
+function Topic(topicName, topicDescription) {
+
+};
---
+++ src/ExampleWebSubscriber/Scripts/HermesTests.js
@@ -0,0 +1,60 @@
+﻿///<reference path="Hermes.js" />
+var serviceUrl = 'http://localhost:6156/';
+
+test("Always passes", function () {
+    ok(true, "Passes");
+});
+
+module("HermesClient");
+
+test("Call HermesClient constructor without new", function () {
+    var hermes = HermesClient(serviceUrl);
+    ok(hermes instanceof HermesClient, "The returned value should be an instance of Hermes");
+});
+
+test("Call HermesClient constructor without serviceUrl", function () {
+    raises(function () { HermesClient(); }, "Calling constructor without a serviceUrl should raise an exception.");
+});
+
+test("Call HermesClient constructor with null serviceUrl", function () {
+    raises(function () { HermesClient(null); }, "Calling constructor a null serviceUrl should raise an exception.");
+});
+
+test("Call HermesClient constructor with empty serviceUrl", function () {
+    raises(function () { HermesClient(''); }, "Calling constructor with an empty serviceUrl should raise an exception.");
+});
+
+module("HermesClient.CreateGroup");
+
+test("hermes.CreateGroup returns a promise", function () {
+    var client = new HermesClient(serviceUrl);
+    var createGroupPromise = client.CreateGroup('junk');
+    ok('done' in createGroupPromise && 'fail' in createGroupPromise, 'CreateGroup should return a promise');
+});
+
+test("hermes.CreateGroup with no name", function () {
+    var client = new HermesClient(serviceUrl);
+    raises(function () { client.CreateGroup(); }, "Calling CreateGroup with no name should throw an exception");
+});
+
+test("hermes.CreateGroup with null name", function () {
+    var client = new HermesClient(serviceUrl);
+    raises(function () { client.CreateGroup(null); }, "Calling CreateGroup with null name should throw an exception");
+});
+
+test("hermes.CreateGroup with empty name", function () {
+    var client = new HermesClient(serviceUrl);
+    raises(function () { client.CreateGroup(''); }, "Calling CreateGroup with empty name should throw an exception");
+});
+
+test("hermes.CreateGroup completes", function () {
+    var client = new HermesClient(serviceUrl);
+    stop();
+
+    client.CreateGroup('hermes.CreateGroup completes')
+        .done(function (group) {
+            ok(true);
+            start();
+        });
+});
+
