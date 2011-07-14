9d8a38435ec79e4b7df1e990a8a6bcb3645455f5
Jason Dentler <jasondentler@gmail.com>
2011-07-14 15:25:13 -0500

    added tests for PostMessage

--- src/JsClient/Scripts/Tests/MessageTests.js
+++ src/JsClient/Scripts/Tests/MessageTests.js
@@ -16,9 +16,52 @@ $(document).ready(function () {
     test('Post a string message', function () {
         var groupName = 'Post a string message';
         var topicName = groupName;
-        var message = 'The crow flies at midnight';
+        var message = 'The crow flies at midnight.';
         usingGroupAndTopic(groupName, topicName, function (group, topic) {
             topic.PostStringMessage(message)
+                .done(function (location) {
+                    start();
+                    notEqual(location, null);
+                    notEqual(location, '');
+                })
+                .fail(function () {
+                    start();
+                    ok(false);
+                });
+        });
+
+    });
+
+    test('Post an xml message', function () {
+        var groupName = 'Post an xml message';
+        var topicName = groupName;
+        var message = '<message>The crow flies at midnight.</message>';
+        usingGroupAndTopic(groupName, topicName, function (group, topic) {
+            topic.PostMessage(message, 'application/xml')
+                .done(function (location) {
+                    start();
+                    notEqual(location, null);
+                    notEqual(location, '');
+                })
+                .fail(function () {
+                    start();
+                    ok(false);
+                });
+        });
+
+    });
+
+    test('Post a json message', function () {
+        var groupName = 'Post an xml message';
+        var topicName = groupName;
+        var msgObject = {
+            message: 'The crow flies at midnight.',
+            timestamp: new Date()
+        };
+        var message = JSON.stringify(msgObject);
+        
+        usingGroupAndTopic(groupName, topicName, function (group, topic) {
+            topic.PostMessage(message, 'application/json')
                 .done(function (location) {
                     start();
                     notEqual(location, null);
