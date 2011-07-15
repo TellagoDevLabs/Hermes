f14437e523948577b2c4008544887db955e13fb4
Silvio <silvio.massari@gmail.com>
2011-07-15 15:34:02 -0300

    Bug fixed: MongoDB service does not uninstall

--- src/Installers/CustomActions/Services.cs
+++ src/Installers/CustomActions/Services.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ServiceProcess;
 using Microsoft.Deployment.WindowsInstaller;
 
@@ -33,11 +34,57 @@ namespace TellagoStudios.Hermes.CustomActions
                 var serviceName = session["START_SERVICE_NAME"];
                 var service = new ServiceController(serviceName);
 
-                service.Stop();
+
+                session.Log(service.Status.ToString());
+                if (service.Status == ServiceControllerStatus.StartPending ||
+                    service.Status == ServiceControllerStatus.Running)
+                {
+                    service.Stop();
+                }
+
                 service.WaitForStatus(ServiceControllerStatus.Stopped, TimeSpan.FromSeconds(30));
             }
             catch (Exception ex)
             {
+                session.Log("CustomActionException: (StopService)" + ex.ToString());
+                return ActionResult.Failure;
+            }
+            return ActionResult.Success;
+        }
+
+        [CustomAction]
+        public static ActionResult StopMongoService(Session session)
+        {
+            try
+            {
+                var serviceName = session["START_SERVICE_NAME"];
+                var service = new ServiceController(serviceName);
+
+                session.Log("SAM");
+                session.Log(service.Status.ToString());
+                if (service.Status == ServiceControllerStatus.StartPending ||
+                    service.Status == ServiceControllerStatus.Running)
+                {
+                    service.Stop();
+                }
+
+                service.WaitForStatus(ServiceControllerStatus.Stopped, TimeSpan.FromSeconds(30));
+            }
+            catch (Win32Exception w32Ex)
+            {
+                session.Log("Win32exception: Code={0}, Native={1}, Message={2}", w32Ex.ErrorCode, w32Ex.NativeErrorCode, w32Ex.Message);
+
+                if (w32Ex.ErrorCode == 109 || w32Ex.NativeErrorCode == 109)
+                {
+                    // it's OK
+                    return ActionResult.Success;
+                }
+                session.Log("CustomActionException: (StopService)" + w32Ex.ToString());
+                return ActionResult.Failure;
+            }
+            catch (Exception ex)
+            {
+                session.Log(ex.GetType().ToString());
                 session.Log("CustomActionException: (StopService)" + ex.ToString());
                 return ActionResult.Failure;
             }
